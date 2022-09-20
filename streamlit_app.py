7716e4088c6976fe33c6e6bd6e14961e31f2ca8f
Supriya <113984225+PRIYAREDDY0906@users.noreply.github.com>
2022-09-20 18:54:41 +0530

    Update streamlit_app.py
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,11 @@ streamlit.header('Breakfast Menu')
 streamlit.text('omega3 &bluebreey oatmeal')
 streamlit.text('spinach and rocket smoothie')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 🥗 🐔 🥑🍞Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
