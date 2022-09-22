c00d9f7a98b365dbb601c10d5c9f17cfe1f78eac
Supriya <113984225+PRIYAREDDY0906@users.noreply.github.com>
2022-09-22 17:45:00 +0530

    Update streamlit_app.py
    added code to display fruityvice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,7 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+#New code to display fruityvice response
+import requests
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon");
+streamlit.text(fruityvice_response);
